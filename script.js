bf7b0aa07a5a7ea0d90611c72af60f9aa3044115
vv1705260-rgb <vv1705260@gmail.com>
2026-04-14 11:20:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function typing() {
 }
 
 typing();
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    padding: 15px 30px;
+    background: rgba(0,0,0,0.3);
+    position: sticky;
+    top: 0;
+}
+
+.navbar a {
+    margin: 0 10px;
+    color: white;
+    text-decoration: none;
+}
+
+.navbar a:hover {
+    color: #f72585;
+}
